8c66e2ef2be984c5d912eb27dec3f4fc51f82922
victornietol <nietovirgo17@gmail.com>
2025-03-14 19:05:32 -0600

    Implementacion de botones para ordenar los elementos a mostrar de ingreso y egresos

--- lib/customWidgets/botones.dart
+++ lib/customWidgets/botones.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:decimal/decimal.dart';
 import 'package:intl/intl.dart';
 import 'package:calculadora_presupuesto/customWidgets/cuadrosDialogo.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 // Boton con previsualizacion del monto del ingreso o egreso
 class BotonIngresoEgreso extends StatefulWidget {
@@ -26,10 +27,17 @@ class BotonIngresoEgreso extends StatefulWidget {
 class _BotonIngresoEgresoState extends State<BotonIngresoEgreso> {
   // Lista de los widgets para los ingreso o egresos
   List<Widget> botones = [];
+  final List<String> _ordenarPorCampo = ['fecha_registro', 'monto', 'nombre'];
+  String? _ordenarPorCampo_Valor = 'fecha_registro';
+  final List<String> _ordenarAscDesc = ['descendente', 'ascendente'];
+  String? _ordenarAscDesc_Valor = 'descendente';
+
+  List<Map<String, dynamic>> _listaElementosMutable = [];
 
   @override
   void initState() {
     super.initState();
+    _listaElementosMutable = List.from(widget.listaElementos); // Hacer mutable la lista para poder ordenarla
     _crearWidgetsTodos();
   }
 
@@ -47,13 +55,37 @@ class _BotonIngresoEgresoState extends State<BotonIngresoEgreso> {
     final formatoDinero = NumberFormat.currency(locale: "es_MX", symbol: "\$");
     return formatoDinero.format(monto);
   }
+
+  void _ordenarListaElementos() {
+    // Determinar si se ordena ascendete o descendente
+    int Function(Map<String, dynamic>, Map<String, dynamic>) comparar;
+    if(_ordenarAscDesc_Valor==_ordenarAscDesc[0]) {
+      comparar = (a, b) => b[_ordenarPorCampo_Valor].compareTo(a[_ordenarPorCampo_Valor]); // descendete
+    } else {
+      comparar = (a, b) => a[_ordenarPorCampo_Valor].compareTo(b[_ordenarPorCampo_Valor]); // ascendente
+    }
+    try {
+      // Realizar la comparacion para determinar el order
+      setState(() {
+        _listaElementosMutable.sort(comparar);
+      });
+
+    } catch (e) {
+      print("Error en la funcion _ordenarListaElementos de la clase _BotonIngresoEgresoState del archivo botones.dart: $e");
+    }
+  }
   
   // Pestaña todos
   void _crearWidgetsTodos() {
     double tamanioTextPrincipal = 20.0;
     botones.clear(); // Evitar duplicados
-      // Recorrer todos los elementos de la BD
-      for (var elemento in widget.listaElementos) {
+
+    // ordenar lista de elementos antes de construir los widgets
+    _ordenarListaElementos();
+
+
+      // Recorrer todos los elementos de la BD y crear un boton para cada elemento
+      for (var elemento in _listaElementosMutable) {
         double porcentaje = (elemento['monto']*100.0) / widget.totalIngresos.toDouble(); // Se utiliza double en lugar de decimal para el porcentaje por temas de precision en el punto decimal que no puede manejar Decimal
         String categoria = obtenerCategoria(elemento['fk_id_categoria_ingreso']);
         String montoFormateado = formatearCantidad(elemento['monto']);
@@ -121,7 +153,7 @@ class _BotonIngresoEgresoState extends State<BotonIngresoEgreso> {
                               child: Text(
                                 '${porcentaje.toStringAsFixed(2)}%',
                                 style: TextStyle(
-                                  color: Colors.green,
+                                  color: widget.tipo == 'ingreso' ? Colors.green : Colors.red,
                                 ),
                               ),
                             ),
@@ -154,11 +186,155 @@ class _BotonIngresoEgresoState extends State<BotonIngresoEgreso> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: botones,
-      ),
+    return Column( // Esto es lo que regresa el widget
+      children: [
+        // Botones de ordenamiento
+        Row(
+          children: [
+            Expanded(
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton2<String>(
+                    isExpanded: true,
+                    hint: Text(
+                      _ordenarPorCampo_Valor ?? 'Fecha registro',
+                      style: const TextStyle(
+                          color: Colors.black
+                      ),
+                    ),
+                    items: _ordenarPorCampo
+                        .map((String item) => DropdownMenuItem<String>(
+                      value: item,
+                      child: Text(
+                        item[0].toUpperCase()+item.substring(1)=='Fecha_registro' ?
+                          'Fecha registro' :
+                          item[0].toUpperCase()+item.substring(1),
+                        style: const TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ))
+                        .toList(),
+                    value: _ordenarPorCampo_Valor,
+                    onChanged: (String? value) {
+                      setState(() {
+                        _ordenarPorCampo_Valor = value;
+                        _crearWidgetsTodos();
+                      });
+                    },
+                    buttonStyleData: const ButtonStyleData(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      height: 40,
+                      width: double.maxFinite,
+                    ),
+                    menuItemStyleData: const MenuItemStyleData(
+                      height: 40,
+                    ),
+                  ),
+                ),
+            ),
+            Expanded(
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton2<String>(
+                    isExpanded: true,
+                    hint: Text(
+                      _ordenarAscDesc_Valor ?? 'Descendente',
+                      style: const TextStyle(
+                          color: Colors.black
+                      ),
+                    ),
+                    items: _ordenarAscDesc
+                        .map((String item) => DropdownMenuItem<String>(
+                      value: item,
+                      child: Text(
+                        item[0].toUpperCase()+item.substring(1),
+                        style: const TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ))
+                        .toList(),
+                    value: _ordenarAscDesc_Valor,
+                    onChanged: (String? value) {
+                      setState(() {
+                        _ordenarAscDesc_Valor = value;
+                        _crearWidgetsTodos();
+                      });
+                    },
+                    buttonStyleData: const ButtonStyleData(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      height: 40,
+                      width: double.maxFinite,
+                    ),
+                    menuItemStyleData: const MenuItemStyleData(
+                      height: 40,
+                    ),
+                  ),
+                ),
+            ),
+          ],
+        ),
+
+        // Lista de botones de elementos
+        Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: botones,
+              ),
+            ),
+        ),
+      ],
     );
+    /*
+      Column(
+      children: [
+        // Boton para ordenamiento
+        DropdownButtonHideUnderline(
+          child: DropdownButton2<String>(
+            isExpanded: true,
+            hint: const Text(
+              'Ordenar por',
+              style: TextStyle(
+                  color: Colors.black
+              ),
+            ),
+            items: _ordenarPorCampo
+                .map((String item) => DropdownMenuItem<String>(
+              value: item,
+              child: Text(
+                item[0].toUpperCase()+item.substring(1),
+                style: const TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ))
+                .toList(),
+            value: _ordenarPorCampo_Valor,
+            onChanged: (String? value) {
+              setState(() {
+                _ordenarPorCampo_Valor = value;
+              });
+            },
+            buttonStyleData: const ButtonStyleData(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              height: 40,
+              width: double.maxFinite,
+            ),
+            menuItemStyleData: const MenuItemStyleData(
+              height: 40,
+            ),
+          ),
+        ),
+
+        // Lista de botones
+        SingleChildScrollView(
+          child: Column(
+          children: botones,
+          ),
+        ),
+      ],
+    );
+
+     */
   }
 
 
