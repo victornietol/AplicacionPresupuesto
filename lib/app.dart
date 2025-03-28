65985b31851e895721d8a462101397e239937660
victornietol <nietovirgo17@gmail.com>
2025-03-28 03:16:42 -0600

    Se agregaron imagenes para pantalla de carga e icono

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Presupuesto',
+      title: 'Gestor Presupuesto',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         useMaterial3: true,
